9c043f59a20cdbe67e29dca15409730551484f42
zi7feng <ziqi_feng7@163.com>
2024-10-23 17:32:25 -0400

    add picture upload, create post

---
+++ src/main/java/com/fzq/xiaopotato/model/dto/post/PostQueryDTO.java
@@ -0,0 +1,17 @@
+package com.fzq.xiaopotato.model.dto.post;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class PostQueryDTO implements Serializable {
+
+    private static final long serialVersionUID = -860411473338993839L;
+    private Integer currentPage = 1;
+    private Integer pageSize = 10;
+    private String postTitle;
+    private String postContent;
+
+
+}
