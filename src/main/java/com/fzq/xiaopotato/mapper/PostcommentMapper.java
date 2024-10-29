02116d872a130e8f9b747cb50a4ddbe1dc2f81c0
zi7feng <ziqi_feng7@163.com>
2024-10-29 18:03:50 -0400

    + comment module + activate comment count in PostVO

---
+++ src/main/java/com/fzq/xiaopotato/mapper/PostcommentMapper.java
@@ -0,0 +1,18 @@
+package com.fzq.xiaopotato.mapper;
+
+import com.fzq.xiaopotato.model.entity.Postcomment;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author zfeng
+* @description 针对表【Postcomment】的数据库操作Mapper
+* @createDate 2024-10-29 12:48:53
+* @Entity com.fzq.xiaopotato.model.entity.Postcomment
+*/
+public interface PostcommentMapper extends BaseMapper<Postcomment> {
+
+}
+
+
+
+
