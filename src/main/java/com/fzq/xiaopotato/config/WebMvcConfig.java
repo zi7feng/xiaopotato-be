ac638f1c2057da5cac1eafe9f32a1c8824ae214f
zi7feng <ziqi_feng7@163.com>
2024-11-20 16:36:13 -0500

    add prod/dev environment

--- src/main/java/com/fzq/xiaopotato/config/WebMvcConfig.java
+++ src/main/java/com/fzq/xiaopotato/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.fzq.xiaopotato.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("!prod")
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
