ecbed45649782c32c802921500e8947e7820b02e
zi7feng <ziqi_feng7@163.com>
2024-11-20 16:52:42 -0500

    add admin delete user by id

--- src/main/java/com/fzq/xiaopotato/config/SocketIOConfig.java
+++ src/main/java/com/fzq/xiaopotato/config/SocketIOConfig.java
@@ -6,6 +6,7 @@ import com.corundumstudio.socketio.Transport;
 import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
 import com.fzq.xiaopotato.common.utils.SocketIOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Map;
@@ -16,15 +17,21 @@ public class SocketIOConfig {
     @Autowired
     private SocketIOUtils socketIOUtils;  // 注入 SocketIOUtils
 
+    @Value("${socketio.host}") // 从配置文件中读取主机名
+    private String host;
+
+    @Value("${socketio.port}") // 从配置文件中读取端口号
+    private int port;
+
     @Bean
     public SocketIOServer socketIOServer() {
         Configuration config = new Configuration();
-//        config.setHostname("localhost");
-//        config.setPort(8081);
-        config.setHostname("0.0.0.0");
-        config.setPort(80);
+        config.setHostname(host);
+        config.setPort(port);
+//        config.setHostname("0.0.0.0");
+//        config.setPort(80);
         config.setOrigin("*");
-        config.setContext("/socket.io");
+//        config.setContext("/socket.io");
         config.setTransports(com.corundumstudio.socketio.Transport.WEBSOCKET, Transport.POLLING);
 
         SocketIOServer server = new SocketIOServer(config);
