91f9d6f8b8fc836d1ad64e53a611ebbd6ed19ffa
Chenley <chenley@qq.com>
2025-01-14 21:26:37 -0500

    update socket io class

--- src/main/java/com/fzq/xiaopotato/config/SocketIOConfig.java
+++ src/main/java/com/fzq/xiaopotato/config/SocketIOConfig.java
@@ -6,17 +6,17 @@ import com.corundumstudio.socketio.Transport;
 import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
 import com.fzq.xiaopotato.common.utils.SocketIOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Map;
 
 @org.springframework.context.annotation.Configuration
 public class SocketIOConfig {
 
-    @Autowired
-    private SocketIOUtils socketIOUtils;  // 注入 SocketIOUtils
+    private final SocketIOUtils socketIOUtils;
 
     @Value("${socketio.host}") // 从配置文件中读取主机名
     private String host;
@@ -24,30 +24,30 @@ public class SocketIOConfig {
     @Value("${socketio.port}") // 从配置文件中读取端口号
     private int port;
 
+    @Autowired
+    public SocketIOConfig(SocketIOUtils socketIOUtils) {
+        this.socketIOUtils = socketIOUtils;
+    }
+
     @Bean
+    @Lazy
     public SocketIOServer socketIOServer() {
         Configuration config = new Configuration();
         config.setHostname(host);
         config.setPort(port);
-//        config.setHostname("0.0.0.0");
-//        config.setPort(80);
         config.setOrigin("*");
-//        config.setContext("/socket.io");
-        config.setTransports(com.corundumstudio.socketio.Transport.WEBSOCKET, Transport.POLLING);
+        config.setTransports(Transport.WEBSOCKET, Transport.POLLING);
 
         SocketIOServer server = new SocketIOServer(config);
 
-        // 注册事件监听器
         server.addConnectListener(socketIOUtils::onConnect);
         server.addDisconnectListener(socketIOUtils::onDisconnect);
         server.addEventListener("message", String.class, (client, data, ackSender) -> {
             socketIOUtils.handleMessage(client, data);
         });
-
         server.addEventListener("pull", String.class, (client, data, ackSender) -> {
             socketIOUtils.handlePullAllNotifications(client);
         });
-
         server.addEventListener("heartbeat", Map.class, (client, data, ackSender) -> {
             socketIOUtils.handleHeartbeat(client);
         });
@@ -56,6 +56,7 @@ public class SocketIOConfig {
     }
 
     @Bean
+    @DependsOn("socketIOServer")
     public SpringAnnotationScanner springAnnotationScanner(SocketIOServer socketIOServer) {
         return new SpringAnnotationScanner(socketIOServer);
     }
